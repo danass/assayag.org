bb5bba0c048af7badfe2f98eb8d5247b233d088e
daniel assayag <hi@monsieurdaniel.com>
2022-03-05 05:33:31 +0100

    Added twitter.json data

--- imports/ui/Twitter.jsx
+++ imports/ui/Twitter.jsx
@@ -1,7 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import React, { useState, useEffect } from "react";
-import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection, TwitterCollection } from '../api/Collection.js';
 import { Tooltip } from './Tooltip';
 
 export const TweetRender = ({ tweet }) => {
@@ -16,10 +14,6 @@ export const TweetRender = ({ tweet }) => {
         return <span key={index}>{word} </span>
       }
     })}
-
-
-
-
 
     {tweet.media ? <img src={tweet.media[0].media_url_https} /> : null}
     <div className="twitter-usernames">{tweet.usernames ? tweet.usernames.map((u, i) => { return <div key={i}>{u}</div> }) : ""}</div>
@@ -39,37 +33,26 @@ export const Twitter = () => {
       let usernames = tweet[0].text.match(/@[a-zA-Z0-9_]+/g);
       tweet[0].usernames = usernames ? usernames : [];
       tweet[0].text = tweet[0].text.replace(/@[a-zA-Z0-9_]+/g, '');
-      // transform text link into a a href link
       setTweet(tweet)
-
     });
   }
 
   const createBoxes = (userOptions) => {
     let boxes = [];
     for (const [key, value] of Object.entries(userOptions)) {
-
-
-        boxes.push(
-          <label key={key}>
+      boxes.push(
+        <label key={key}>
           <input type="checkbox" checked={value} onChange={(e) => {
             let newOptions = { ...userOptions };
-            
             newOptions[key] = e.target.checked;
-            newOptions['insta'] = false
-            console.log(newOptions)
             setuserOptions(newOptions);
-
           }} />
           {key}
-
         </label>
-        );
-      
+      );
     }
     return boxes;
   }
-
 
   const handleClick = () => {
     fetchData(userOptions)
@@ -102,50 +85,11 @@ export const Twitter = () => {
         </Tooltip>
       </ul>
       // create an input checkbox to update each key of userOptions
-          
+
 
 
       <div>
         {createBoxes(userOptions)}
-
-        {/* <label>
-          <input type="checkbox" checked={userOptions.insta} onChange={(e) => {
-            setuserOptions({ ...userOptions, insta: e.target.checked })
-          }} />
-          Instagram
-        </label>
-        <label>
-          <input type="checkbox" checked={userOptions.twitter} onChange={(e) => {
-            setuserOptions({ ...userOptions, twitter: e.target.checked })
-          }} />
-          Twitter
-        </label>
-        <label>
-          <input type="checkbox" checked={userOptions.youtube} onChange={(e) => {
-            setuserOptions({ ...userOptions, youtube: e.target.checked })
-          }} />
-          Youtube
-        </label>
-        <label>
-          <input type="checkbox" checked={userOptions.google} onChange={(e) => {
-            setuserOptions({ ...userOptions, google: e.target.checked })
-          }} />
-          Google
-        </label>
-        <label>
-          <input type="checkbox" checked={userOptions.tumblr} onChange={(e) => {
-            setuserOptions({ ...userOptions, tumblr: e.target.checked })
-          }} />
-          Tumblr
-        </label>
-        <label>
-          <input type="checkbox" checked={userOptions.assayag} onChange={(e) => {
-            setuserOptions({ ...userOptions, assayag: e.target.checked })
-          }} />
-          Assayag
-        </label> */}
-
-
       </div>
     </div>
   );
