6f1853b1b13077e3de0dd77769d2f00a15667f8f
daniel assayag <hi@monsieurdaniel.com>
2022-03-16 19:00:12 +0100

    term-of-use update

--- assets/ui/static/Terms.jsx
+++ assets/ui/static/Terms.jsx
@@ -9,7 +9,7 @@ export const Terms = () => {
                         <h1><strong>Terms and Conditions</strong></h1>
 
                     </div>
-
+                    <h2>Terms of use</h2>
                     <p>Welcome to <b>Assayag.org</b>!</p>
 
                     <p>These terms and conditions outline the rules and regulations for the use of Assayag.org's Website, located at <b>https://www.assayag.org/terms-of-use</b>.</p>
@@ -103,6 +103,36 @@ export const Terms = () => {
 
                     <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
 
+                    <h3><strong>Your Privacy</strong></h3>
+
+                    <p>Please read Privacy Policy</p>
+
+                    <h3><strong>Reservation of Rights</strong></h3>
+
+                    <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+
+                    <h3><strong>Removal of links from our website</strong></h3>
+
+                    <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+
+                    <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+
+                    <h3><strong>Disclaimer</strong></h3>
+
+                    <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+
+                    <ul>
+                        <li>limit or exclude our or your liability for death or personal injury;</li>
+                        <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+                        <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+                        <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+                    </ul>
+
+                    <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
+
+                    <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+                    <div style={{ padding: '25px'}}></div>
+                    <div className="main-container-header-title" ><h1>Privacy Policy</h1></div>
                     <h2>Privacy Policy for Assayag.org</h2>
 
                     <p>At Assayag.org, accessible from https://www.assayag.org, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Assayag.org and how we use it.</p>
@@ -160,30 +190,7 @@ export const Terms = () => {
 
                     <p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
 
-                    <h3><strong>Reservation of Rights</strong></h3>
-
-                    <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
-
-                    <h3><strong>Removal of links from our website</strong></h3>
-
-                    <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
-
-                    <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
-
-                    <h3><strong>Disclaimer</strong></h3>
-
-                    <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
-
-                    <ul>
-                        <li>limit or exclude our or your liability for death or personal injury;</li>
-                        <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
-                        <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
-                        <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
-                    </ul>
-
-                    <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
-
-                    <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+                    
                 </div>
             </div>
         </>
